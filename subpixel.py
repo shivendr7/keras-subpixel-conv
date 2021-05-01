c898d05e784004a7081ce3cedc19454ec0123690
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-05-01 19:28:12 +0530

    Update subpixel.py
    updated for tensorflow 2.4.1

--- subpixel.py
+++ subpixel.py
@@ -25,7 +25,7 @@ def SubpixelConv2D(input_shape, scale=4):
         return output_shape
 
     def subpixel(x):
-        return tf.depth_to_space(x, scale)
+        return tf.nn.depth_to_space(x, scale)
 
 
     return Lambda(subpixel, output_shape=subpixel_shape, name='subpixel')
